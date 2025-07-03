032a81e2ff6df13794c1be07d4c7512c4936df78
EliasCarvalho20 <elias_carvalho2050@outlook.com>
2025-07-02 21:24:34 -0300

    movendo diretorios

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -13,6 +13,10 @@ def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
     Returns:
         Um dicionário com as notas da escala e os graus.
 
+    Raises:
+        ValueError: Caso a tônica não seja uma nota valida.
+        KeyError: Caso a escala não exista ou não tenha sido implementada.
+
     Examples:
         >>> escala('C', 'maior')
         {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
@@ -20,8 +24,17 @@ def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
         >>> escala('a', 'menor')
         {'notas': ['A', 'B', 'C', 'D', 'E', 'F', 'G'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
     """
-    intervalos = ESCALAS[tonalidade]
-    tonica_pos = NOTAS.index(tonica)
+    tonica = tonica.upper()
+    try:
+        tonica_pos = NOTAS.index(tonica)
+        intervalos = ESCALAS[tonalidade]
+    except ValueError:
+        raise ValueError(f"Essa nota não existe, tente uma dessas {NOTAS}")
+    except KeyError:
+        raise KeyError(
+            "Essa escala não existe ou não foi implementada. "
+            f"Tente uma dessas {list(ESCALAS.keys())}"
+        )
 
     temp = [NOTAS[(tonica_pos + i) % 12] for i in intervalos]
     return {"notas": temp, "graus": ["I", "II", "III", "IV", "V", "VI", "VII"]}
